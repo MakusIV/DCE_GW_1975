2e7658332aaec1e1a77e49e9b8f756e78381662d
Marco Bellafante <marco.bellafante@gmail.com>
2022-11-28 20:54:06 +0100

    insert logging and testing code functionality

---
+++ DCS_SavedGames_Path/Mods/tech/DCE/Missions/Campaigns/1975 Georgian War/Debug/scen_destroyed.13.lua
@@ -0,0 +1,88 @@
+--Destroyed scenery objects
+
+scen_log = {
+	[98239493] = {
+		['lasthit'] = 'Pack 2 - F9 - Strike 1-3',
+		['health0'] = 500,
+	},
+	[136776302] = {
+		['lasthit'] = 'Pack 13 - 1./81.IAP - Strike 1-1',
+		['health0'] = 500,
+	},
+	[164169500] = {
+		['lasthit'] = 'Pack 2 - F9 - Strike 1-3',
+		['health0'] = 500,
+	},
+	[104860090] = {
+		['y'] = 648.62872314453,
+		['x'] = -70066.166503906,
+		['lasthit'] = '206 SA-2 Site B-3-2',
+		['z'] = 693080.74658203,
+		['health0'] = 500,
+	},
+	[73730574] = {
+		['lasthit'] = 'Pack 15 - 1./81.IAP - Strike 1-1',
+		['health0'] = 500,
+	},
+	[98238784] = {
+		['lasthit'] = 'Pack 2 - F9 - Strike 1-3',
+		['health0'] = 500,
+	},
+	[73730608] = {
+		['lasthit'] = 'Pack 15 - 1./81.IAP - Strike 1-1',
+		['health0'] = 500,
+	},
+	[73237688] = {
+		['y'] = 462.21606445312,
+		['x'] = -299301.21875,
+		['lasthit'] = 'Pack 10 - 1./135.IAP - Strike 1-2',
+		['z'] = 872987.375,
+		['health0'] = 500,
+	},
+	[72878059] = {
+		['lasthit'] = 'Pack 8 - 1./115.IAP - Strike 1-2',
+		['health0'] = 500,
+	},
+	[161778236] = {
+		['lasthit'] = 'Sochi Defenses-2',
+		['health0'] = 500,
+	},
+	[136776289] = {
+		['lasthit'] = 'Pack 13 - 1./81.IAP - Strike 1-1',
+		['health0'] = 500,
+	},
+	[259916726] = {
+		['lasthit'] = 'Pack 13 - 1./81.IAP - Strike 1-1',
+		['health0'] = 500,
+	},
+	[259916727] = {
+		['lasthit'] = 'Pack 13 - 1./81.IAP - Strike 1-1',
+		['health0'] = 500,
+	},
+	[96962636] = {
+		['lasthit'] = 'Pack 12 - GA 7rd AS - Strike 1-1',
+		['health0'] = 500,
+	},
+	[74058684] = {
+		['y'] = 380.54498291016,
+		['x'] = -144986.53125,
+		['lasthit'] = 'Pack 22 - VF-118/GA - Strike 1-2',
+		['z'] = 816502.625,
+		['health0'] = 500,
+	},
+	[72911065] = {
+		['y'] = 102.96564483643,
+		['x'] = -282606.98828125,
+		['lasthit'] = 'Pack 11 - 1./135.IAP - Strike 1-1',
+		['z'] = 704785.0078125,
+		['health0'] = 500,
+	},
+	[98239508] = {
+		['lasthit'] = 'Pack 2 - F9 - Strike 1-3',
+		['health0'] = 500,
+	},
+	[72878006] = {
+		['lasthit'] = 'Pack 8 - 1./115.IAP - Strike 1-1',
+		['health0'] = 500,
+	},
+}
